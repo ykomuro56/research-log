97a1bcb65badc04d51f792c8939e809969429037
小室雄暉 <komuroyuuki@MacBook-Air-2.local>
2026-06-30 14:22:44 +0900

    feat: create first Spring Boot REST API

---
+++ backend/src/main/java/com/ykomuro/researchlog/ResearchlogApplication.java
@@ -0,0 +1,13 @@
+package com.ykomuro.researchlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchlogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ykomuro/researchlog/ResearchlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykomuro.researchlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
